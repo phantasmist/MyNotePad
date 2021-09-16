e5d78888a29480844566dbf2552b8a0ec6379042
phantasmist <62749624+phantasmist@users.noreply.github.com>
2021-09-16 09:40:00 +0900

    initial upload

---
+++ MyNotePad/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace MyNotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MNToLower = new System.Windows.Forms.ToolStripMenuItem();
+            this.MNToUpper = new System.Windows.Forms.ToolStripMenuItem();
+            this.MNToHex = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.replaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            this.MNReturn = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(661, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.printPreviewToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem,
+            this.replaceToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MNToLower,
+            this.MNToUpper,
+            this.MNToHex,
+            this.MNReturn});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // printPreviewToolStripMenuItem
+            // 
+            this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.printPreviewToolStripMenuItem.Text = "Print Preview";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // MNToLower
+            // 
+            this.MNToLower.Name = "MNToLower";
+            this.MNToLower.Size = new System.Drawing.Size(224, 26);
+            this.MNToLower.Text = "Lowercase";
+            this.MNToLower.Click += new System.EventHandler(this.MNToLower_Click);
+            // 
+            // MNToUpper
+            // 
+            this.MNToUpper.Name = "MNToUpper";
+            this.MNToUpper.Size = new System.Drawing.Size(224, 26);
+            this.MNToUpper.Text = "Uppercase";
+            this.MNToUpper.Click += new System.EventHandler(this.MNToUpper_Click);
+            // 
+            // MNToHex
+            // 
+            this.MNToHex.Name = "MNToHex";
+            this.MNToHex.Size = new System.Drawing.Size(224, 26);
+            this.MNToHex.Text = "Hexadecimal";
+            this.MNToHex.Click += new System.EventHandler(this.MNToHex_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.findToolStripMenuItem.Text = "Find";
+            // 
+            // replaceToolStripMenuItem
+            // 
+            this.replaceToolStripMenuItem.Name = "replaceToolStripMenuItem";
+            this.replaceToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.replaceToolStripMenuItem.Text = "Replace";
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMemo.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMemo.Location = new System.Drawing.Point(0, 33);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.Size = new System.Drawing.Size(661, 315);
+            this.tbMemo.TabIndex = 1;
+            this.tbMemo.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbMemo_KeyUp);
+            // 
+            // MNReturn
+            // 
+            this.MNReturn.Name = "MNReturn";
+            this.MNReturn.Size = new System.Drawing.Size(224, 26);
+            this.MNReturn.Text = "Return";
+            this.MNReturn.Click += new System.EventHandler(this.MNReturn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 349);
+            this.Controls.Add(this.tbMemo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem replaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MNToLower;
+        private System.Windows.Forms.ToolStripMenuItem MNToUpper;
+        private System.Windows.Forms.ToolStripMenuItem MNToHex;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbMemo;
+        private System.Windows.Forms.ToolStripMenuItem MNReturn;
+    }
+}
+
---
+++ MyNotePad/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //전역변수의 초기화
+        string strOrigin = "";
+        int viewState = 0; //0:Normal, 1.Lower, 2.Upper, 3.Hexa
+        private void MNToLower_Click(object sender, EventArgs e)
+        {
+            //string str = tbMemo.Text.ToLower();
+            //tbMemo.Text = str;
+            if(viewState != 1) //Lower View가 아니라면..
+            { 
+                if(strOrigin == "") strOrigin = tbMemo.Text; //원판보존
+                tbMemo.Text = strOrigin.ToLower(); //현재 화면이 아니라 원판을 기준으로 출력
+                tbMemo.ReadOnly = true;
+                viewState = 1;
+            }
+            
+        }
+
+        private void MNToUpper_Click(object sender, EventArgs e)
+        {
+            if (viewState != 2) //Upper View가 아니라면..
+            {
+                if (strOrigin == "") strOrigin = tbMemo.Text;
+                tbMemo.Text = strOrigin.ToUpper(); 
+                tbMemo.ReadOnly = true;
+                viewState = 2;
+            }
+        }
+
+        private void MNToHex_Click(object sender, EventArgs e)
+        {
+            if (viewState != 3) //Hexa View가 아니라면..
+            {
+                if (strOrigin == "") strOrigin = tbMemo.Text;
+                tbMemo.Text = "";
+                char[] chArr = strOrigin.ToCharArray(); // 꼭 한개의 캐릭터로 떨어지진 않음
+                byte[] bArr = Encoding.Default.GetBytes(chArr); //이걸 안쓰면 한글(2byte)은 짤림
+
+                for (int i = 0; i < bArr.Length; i++)
+                {
+                    //{}:manipulator, format, 변환문자
+                    //C#의 char값은 정수형과 호환되지 않는다
+                    //2X가 아니라 X2임을 명심할 것
+                    //tbMemo.Text += string.Format("{0:X2} ", (byte)strOrigin[i]);
+                    //위 코드와 비교하면, encoding을 거치지 않으면 2byte 문자가 손실됨
+                    tbMemo.Text += $"{bArr[i]:X2} "; //$" ": 보간 문자열(String Interpolation)
+                    if (i % 16 == 15) tbMemo.Text += "\r\n";
+                }
+                //int cnt = 0;
+                //foreach (byte ch in strOrigin)
+                //{
+                //    tbMemo.Text += string.Format("{0:2X} ", ch);
+                //    if (cnt++ % 16 == 15) tbMemo.Text += "\r\n";
+                //}
+                tbMemo.ReadOnly = true;
+                viewState = 3;
+            }
+          
+        }
+
+        private void MNReturn_Click(object sender, EventArgs e)
+        {
+            if (tbMemo.ReadOnly == true) // 이거 없으면 빈칸으로 만들수도 있다
+            {
+                tbMemo.Text = strOrigin;
+                strOrigin = "";
+                tbMemo.ReadOnly = false;
+                viewState = 0;
+            }
+        }
+
+        private void tbMemo_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                MNReturn_Click(sender, e);
+            }
+        }
+    }
+}
